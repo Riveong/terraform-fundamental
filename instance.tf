3542a914026560dfcb44aaff3818c1b52454bc9a
Ong Riverine <53504975+Riveong@users.noreply.github.com>
2023-04-28 19:23:25 +0700

    Create instance.tf

---
+++ instance.tf
@@ -0,0 +1,16 @@
+resource "google_compute_instance" "terraform" {
+  project      = "project-aing"
+  name         = "terraform"
+  machine_type = "n1-standard-1"
+  zone         = "us-west1-c"
+  boot_disk {
+    initialize_params {
+      image = "debian-cloud/debian-11"
+    }
+  }
+  network_interface {
+    network = "default"
+    access_config {
+    }
+  }
+}
